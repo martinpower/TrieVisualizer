cf5d29536c650a36056412c2c4f9104ea7798a74
martinpower <power.martin@outlook.com>
2018-11-27 12:47:02 -0500

    refactored

--- Scripts/TrieNode.js
+++ Scripts/TrieNode.js
@@ -2,29 +2,23 @@ class TrieNode
 {
 	constructor(count, coord, scene, ch)
 	{
-		console.log("coord x: " + coord.getComponent(0));
-		console.log("coord y: " + coord.getComponent(1));
-		console.log("coord z: " + coord.getComponent(2));
 		this.count = 0;
 		this.children = new Array(26);
 		this.child_lines = new Array(26);
 
-		let cyl = new THREE.CylinderGeometry(.5, .5, 1, 50);
-		cyl.applyMatrix(new THREE.Matrix4().makeTranslation(coord.getComponent(0), coord.getComponent(1), coord.getComponent(2)));
 		this.coord = coord;
 
-		let col = 10000 + (10000000 / 25) * ch;
-		this.cylinder = new THREE.Mesh(cyl, new THREE.MeshBasicMaterial({color: col}));
-		scene.add(this.cylinder);
+		this.createCylinder(ch, scene);
 	}
 
 	insertWord(word, scene, idx)
 	{
 		// at root of trie
 		if(idx == undefined)
+		{
+			console.log(word);
 			return this.insertWord(word, scene, 0);
-		console.log(idx);
-		console.log(word.charAt(idx));
+		}
 
 		let ch = word.charCodeAt(idx) - "a".charCodeAt(0);
 		let coord = this.computePosition(ch);
@@ -36,8 +30,7 @@ class TrieNode
 		else
 		{
 			this.children[ch] = new TrieNode(0, coord, scene, ch);
-			this.child_lines[ch] = this.createLine(this.children[ch]);
-			scene.add(this.child_lines[ch]);
+			this.child_lines[ch] = this.createLine(this.children[ch], scene);
 		}
 
 		if(idx == word.length)
@@ -55,13 +48,26 @@ class TrieNode
 	{
 		let pos = new THREE.Vector3();
 		pos.y = this.coord.getComponent(1) - 4;
-		pos.x = this.coord.getComponent(0) + 2 * Math.cos(((2 * Math.PI) / 25) * ch);
-		pos.z = this.coord.getComponent(2) + 2 * Math.sin(((2 * Math.PI) / 25) * ch);
 
+		let map = ((2 * Math.PI) / 25) * ch;
+		pos.x = this.coord.getComponent(0) + 3 * Math.sin(map);
+		pos.z = this.coord.getComponent(2) + 3 * Math.cos(map);
+
+		console.log(pos.x + ", " + pos.z);
 		return pos;
 	}
 
-	createLine(child)
+	createCylinder(ch, scene)
+	{
+		let cyl = new THREE.CylinderGeometry(.5, .5, 1, 50);
+		cyl.applyMatrix(new THREE.Matrix4().makeTranslation(this.coord.getComponent(0), this.coord.getComponent(1), this.coord.getComponent(2)));
+
+		let col = 10000 + (10000000 / 25) * ch;
+		this.cylinder = new THREE.Mesh(cyl, new THREE.MeshBasicMaterial({color: col}))
+		scene.add(this.cylinder);
+	}
+
+	createLine(child, scene)
 	{
 		let parentCenter = this.coord.clone();
 		parentCenter.setY(parentCenter.getComponent(1) - 0.5);
@@ -73,6 +79,9 @@ class TrieNode
 
 		let geom = new THREE.TubeGeometry(line, 1, .1);
 
-		return new THREE.Mesh(geom, new THREE.MeshBasicMaterial({color: 0xFFFFFF}));
+		line = new THREE.Mesh(geom, new THREE.MeshBasicMaterial({color: 0xFFFFFF}));
+		scene.add(line);
+
+		return line;
 	}
 }
